b8a37e692c92237a81e681588b072c7afa8fcb6d
alessiapicciuto <picciutoalessia@gmail.com>
2025-12-12 11:13:54 +0100

    Fase 2 completa: Backend stabile e logica CRUD (Create, Read, Update) implementata nei componenti Producer, Factory e Consumer.

---
+++ passaporto-blockchain/hardhat.config.cjs
@@ -0,0 +1,13 @@
+// File: hardhat.config.js
+require("@nomicfoundation/hardhat-ethers");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.28", 
+  networks: {
+    ganache: {
+      url: "http://127.0.0.1:7545", // La tua porta Ganache verificata
+      chainId: 1337, 
+    },
+  },
+};
---
+++ passaporto-blockchain/scripts/deploy.cjs
@@ -0,0 +1,17 @@
+// File: scripts/deploy.js
+const hre = require("hardhat");
+
+async function main() {
+  const ProductPassport = await hre.ethers.getContractFactory("ProductPassport");
+  
+  const productPassport = await ProductPassport.deploy();
+
+  console.log("Contratto ProductPassport deployato all'indirizzo:", productPassport.target);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
